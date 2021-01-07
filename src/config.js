1d325605933ab8f15b95a01e7005f9ab5bca32d9
danita240697 <deysedayane.o.c@gmail.com>
2021-01-06 23:15:01 -0300

    Email Sendgrid fix

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 global.SALT_KEY = "bgJwb15CpSwnmjMHfAAUhtnl2bVZjZJ8";
-global.EMAIL_TMPL = 'Olá, <strong>{0}</strong>, seja bem vindo ao teste da 2ª Etapa da Salvus';
-global.SENDGRID_KEY = 'SG.v_ZB69b14QciI9QuZp0QhYQ'
+global.EMAIL_TMPL = 'Olá, <strong>-body-</strong>, seja bem vindo ao teste da 2ª Etapa da Salvus';
+global.SENDGRID_KEY = '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'
 module.exports = {
     connectionString: "mongodb+srv://deecarneiro:deecarneiro@nodestr-muayt.mongodb.net/test?retryWrites=true&w=majority",
-    sendgridKey: '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',
+    sendgridKey: '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',
 }
--- src/services/email-service.js
+++ src/services/email-service.js
@@ -1,25 +1,24 @@
-'use strict';
+'use strict'
 
-const config = require('../config');
-const sgMail = require('@sendgrid/mail');
+const config = require('../config')
+const sgMail = require('@sendgrid/mail')
 
-sgMail.setApiKey(global.SENDGRID_KEY);
+sgMail.setApiKey(config.sendgridKey)
 
-exports.send = async(to, subject, body) =>{
-    const msg = {
-        to: to, // Change to your recipient
-        from: 'test@example.com', // Change to your verified sender
-        subject: subject,
-        text: body,
-        html: global.EMAIL_TMPL,
-      }
-      sgMail
-      .send(msg)
-      .then(() => {
-        console.log('Email sent')
-      })
-      .catch((error) => {
-        console.error(error)
-      })
-
-};
+exports.send = async (to, subject, body) => {
+  const msg = {
+    to: to, // Change to your recipient
+    from: 'deysedayane.o.c@gmail.com', // Change to your verified sender
+    subject: subject,
+    text: body,
+    html: `<strong>${body}</strong>, seja bem vindo ao teste da 2ª Etapa da Salvus`
+  }
+  sgMail
+    .send(msg)
+    .then(() => {
+      console.log('Email sent')
+    })
+    .catch(error => {
+      console.error(error)
+    })
+}
